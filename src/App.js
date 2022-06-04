389f7a09b87e139504c250197c15acab9365f8c2
horsemaker <yash_ghodekar@yahoo.in>
2022-06-04 18:14:39 +0530

    add `Main` & `ScrollToTop`

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { ScrollToTop } from "./frontend/components";
+import { Main } from "./frontend/layouts";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ScrollToTop>
+        <Main />
+      </ScrollToTop>
+    </div>
+  );
 }
 
 export default App;
