695f27ecc8df1fe213f92d8a8d9a0033ac8ce3cf
horsemaker <yash_ghodekar@yahoo.in>
2022-06-06 01:13:30 +0530

    wrap `Provider`

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import "@fontsource/raleway/800.css";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { theme } from "./frontend/themes";
 import { makeServer } from "./server";
-import "@fontsource/raleway/800.css";
+import { store } from "./frontend/store";
+import { theme } from "./frontend/themes";
 
 makeServer();
 
@@ -13,8 +15,10 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <App />
+        <Provider store={store}>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <App />
+        </Provider>
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>,
