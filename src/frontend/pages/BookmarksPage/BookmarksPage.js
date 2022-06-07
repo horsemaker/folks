df33f42f086e137a8ded0b5145f442f5babb1f1e
horsemaker <yash_ghodekar@yahoo.in>
2022-06-07 22:24:45 +0530

    refactor: dynamic bookmarks

--- src/frontend/pages/BookmarksPage/BookmarksPage.js
+++ src/frontend/pages/BookmarksPage/BookmarksPage.js
@@ -1,10 +1,73 @@
-import { Flex, Heading, useColorModeValue } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  Spinner,
+  useColorModeValue,
+  useToast,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { PostsSection, SuggestedProfiles } from "../../components";
+import { getBookmarkPosts } from "../../features";
 
 const BookmarksPage = () => {
+  const toast = useToast();
+
+  const { token } = useSelector((state) => state.auth);
+  const {
+    data: bookmarks,
+    loading,
+    error,
+  } = useSelector((state) => state.bookmarks);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBookmarkPosts(token));
+  }, [dispatch, token]);
+
+  useEffect(() => {
+    if (error !== "") {
+      toast({
+        title: error.title,
+        description: error.description,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  }, [error, toast]);
+
   return (
     <Flex gap={"12"} position={"relative"}>
-      <PostsSection />
+      {bookmarks.length === 0 ? (
+        loading ? (
+          <Flex
+            direction={"column"}
+            flexGrow={"1"}
+            alignItems={"center"}
+            justifyContent={"center"}
+          >
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="purple.500"
+              size="xl"
+            />
+          </Flex>
+        ) : (
+          <Flex
+            direction={"column"}
+            flexGrow={"1"}
+            alignItems={"center"}
+            justifyContent={"center"}
+          >
+            No Bookmarks
+          </Flex>
+        )
+      ) : (
+        <PostsSection posts={bookmarks} />
+      )}
       <Flex
         display={{ base: "none", lg: "flex" }}
         position={"sticky"}
