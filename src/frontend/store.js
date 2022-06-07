2059126742838edc3366f30e580f678ec3b64c98
horsemaker <yash_ghodekar@yahoo.in>
2022-06-07 19:32:10 +0530

    add reducers

--- src/frontend/store.js
+++ src/frontend/store.js
@@ -1,9 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authReducer } from "./features";
+import {
+  authReducer,
+  bookmarksReducer,
+  postsReducer,
+  userReducer,
+  usersReducer,
+} from "./features";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    bookmarks: bookmarksReducer,
+    posts: postsReducer,
+    user: userReducer,
+    users: usersReducer,
   },
 });
 
