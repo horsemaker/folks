2b88d26eee57dba5e5f05836bf25cfcf57507bee
horsemaker <yash_ghodekar@yahoo.in>
2022-06-08 13:29:57 +0530

    feat: delete & edit post

--- src/frontend/components/Post.js
+++ src/frontend/components/Post.js
@@ -1,15 +1,27 @@
 import { Icon } from "@chakra-ui/icons";
 import {
   Avatar,
+  Button,
   Flex,
   IconButton,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Text,
+  Textarea,
   useColorModeValue,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { FaCircle } from "react-icons/fa";
 import {
   MdBookmark,
@@ -25,16 +37,21 @@ import {
 } from "react-icons/md";
 import Moment from "react-moment";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import {
   bookmarkPost,
+  deletePost,
   dislikePost,
+  editPost,
   likePost,
   removePostFromBookmark,
 } from "../features";
 
 const Post = ({ post }) => {
   const colorModeValue = useColorModeValue(true, false);
+  const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = useRef(null);
 
   const {
     _id,
@@ -48,9 +65,14 @@ const Post = ({ post }) => {
     comments,
   } = post;
 
+  const [postData, setPostData] = useState({ content });
+  const [isEditing, setIsEditing] = useState(false);
+
   const { token, user } = useSelector((state) => state.auth);
   const { data: bookmarks } = useSelector((state) => state.bookmarks);
   const dispatch = useDispatch();
+
+  const { pathname } = useLocation();
 
   const handleLikePost = () => {
     dispatch(likePost({ postId: _id, token }));
@@ -66,6 +88,50 @@ const Post = ({ post }) => {
 
   const handleRemovePostFromBookmark = () => {
     dispatch(removePostFromBookmark({ postId: _id, token }));
+  };
+
+  const handleEditPost = async () => {
+    if (postData.content !== "") {
+      setIsEditing(true);
+      const response = await dispatch(
+        editPost({ postId: _id, postData, token })
+      );
+      if (response?.payload?.posts !== undefined) {
+        toast({
+          title: "Post Edited!",
+          description: "Your post has been edited successfully.",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+        onClose();
+      }
+      setIsEditing(false);
+    } else {
+      toast({
+        title: "Empty Post!",
+        description: "Post can't be left empty.",
+        status: "warning",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const handleDeletePost = async () => {
+    if (pathname === "/bookmarks") {
+      dispatch(removePostFromBookmark({ postId: _id, token }));
+    }
+    const response = await dispatch(deletePost({ postId: _id, token }));
+    if (response?.payload?.posts !== undefined) {
+      toast({
+        title: "Post Deleted!",
+        description: "Your post has been deleted successfully.",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
   };
 
   return (
@@ -114,8 +180,12 @@ const Post = ({ post }) => {
                 bg={colorModeValue ? "white" : "gray.900"}
                 borderColor={colorModeValue ? "gray.200" : "gray.700"}
               >
-                <MenuItem icon={<MdEdit />}>Edit Post</MenuItem>
-                <MenuItem icon={<MdDelete />}>Delete Post</MenuItem>
+                <MenuItem icon={<MdEdit />} onClick={onOpen}>
+                  Edit Post
+                </MenuItem>
+                <MenuItem icon={<MdDelete />} onClick={handleDeletePost}>
+                  Delete Post
+                </MenuItem>
               </MenuList>
             </Menu>
           )}
@@ -193,6 +263,64 @@ const Post = ({ post }) => {
           </Flex>
         </Flex>
       </Flex>
+      {/* Modal */}
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        initialFocusRef={initialRef}
+        isCentered
+        scrollBehavior={"inside"}
+      >
+        <ModalOverlay />
+        <ModalContent bg={useColorModeValue("white", "gray.900")}>
+          <ModalHeader>Editing Post</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Flex direction={"column"} gap={"3"}>
+              <Flex gap={2}>
+                <Avatar
+                  name={`${firstName} ${lastName}`}
+                  src={avatarURL}
+                  alt={username}
+                />
+                <Flex direction={"column"} fontSize={"sm"}>
+                  <Text fontWeight={600}>{`${firstName} ${lastName}`}</Text>
+                  <Text color={"gray.500"}>@{username}</Text>
+                </Flex>
+              </Flex>
+              <Textarea
+                ref={initialRef}
+                placeContent={"What's happening?"}
+                value={postData.content}
+                onChange={(e) =>
+                  setPostData({ ...postData, content: e.target.value })
+                }
+                onFocus={(e) => {
+                  const val = e.target.value;
+                  e.target.value = "";
+                  e.target.value = val;
+                }}
+              />
+            </Flex>
+          </ModalBody>
+          <ModalFooter>
+            <Flex gap={"2"}>
+              <Button colorScheme={"purple"} variant="ghost" onClick={onClose}>
+                Cancel
+              </Button>
+              <Button
+                isLoading={isEditing}
+                colorScheme={"purple"}
+                mr={3}
+                onClick={handleEditPost}
+              >
+                Save
+              </Button>
+            </Flex>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      {/* Modal */}
     </Flex>
   );
 };
